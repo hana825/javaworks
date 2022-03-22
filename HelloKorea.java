65db9da8494387d87780373455e4c4136d012a32
hana825 <hana825@naver.com>
2022-03-22 09:56:53 +0900

    first

---
+++ HelloKorea.java
@@ -0,0 +1,11 @@
+/*
+Mulity line This is comment
+*/
+// Single line Comment
+
+public class HelloKorea {
+	public static void main(String[] args){
+		System.out.println("HelloKorea");
+	}
+
+}
---
+++ Java_00_Hello/src/com/callor/hello/HelloKorea.java
@@ -0,0 +1,19 @@
+/*
+ * 현재 생성한 HelloKorea 클래스는
+ * com/callor/hello 폴더에 저장되어있다 라는 표식
+ * 현재 생성한 클래스가 저장된 base package다 라고 한다.
+ */
+package com.callor.hello;
+
+// 지금 작성하는 소스코드는 HelloKorea.java 파일이다
+// HelloKorea.java를 클래스파일 또는 자바소스코드라고 한다.
+public class HelloKorea {
+	/*
+	 * main 키워드 +[Ctrl] + [Space] : 상용구 작성
+	 * 주황글자(public, class, static, void): Keyword, Reserved word(예약어), Command word(명령어)
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}
