53333fac569a966498ae78d80762057c38e8d7d0
hana825 <hana825@naver.com>
2022-04-01 11:51:34 +0900

    Operator, CasCading

--- Java_20_Control_01/src/com/callor/method/Method_02.java
+++ Java_20_Control_01/src/com/callor/method/Method_02.java
@@ -3,6 +3,11 @@ package com.callor.method;
 public class Method_02 {
 	public static void main(String[] args) {
 		
+		/**********************************************************
+		 * 명령문 해석
+		 * 1. num()를 실행하여 => num()를 호출(Call)하여 실행하고
+		 * 2. num()가 return하는 데이터를 intNum1 변수에 저장하라
+		 **********************************************************/
 		int intNum1 = num();
 		double douNum1 = doNum();
 		long longNum1 = longNum();
