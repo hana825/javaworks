59f4d987eb0f76b17517fc1fddfb9cc521a59203
hana825 <hana825@naver.com>
2022-05-09 15:35:10 +0900

    2022-05-09

---
+++ Java_60_App_02/src/com/callor/app/service/ScoreService.java
@@ -0,0 +1,11 @@
+package com.callor.app.service;
+
+// 클래스의 한 종류이지만 모든 method의 구현제가 없는 상태
+// method의 이름만 정의된 상태
+// method의 prototype만 정의된 상태
+public interface ScoreService {
+	
+	public void inputScore();
+	public void saveScore();
+
+}
